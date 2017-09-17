9b62ed6ea65b4dc9860e53b90a9e156c1be58941
MarionH68 <m.herrgott@hotmail.fr>
2017-09-18 00:14:16 +0200

    modification script SQL
    insertion valeurs dans tabletodelete

--- scriptCreationBaseTableEtInsertionDonnees.sql
+++ scriptCreationBaseTableEtInsertionDonnees.sql
@@ -19,6 +19,8 @@ CREATE TABLE IF NOT EXISTS Banque.tableToDelete
 	id VARCHAR(40) PRIMARY KEY NOT NULL
 );
 
+INSERT INTO `Banque`.`tableToDelete` VALUES ('123456'),('234567'),('345678');
+
 INSERT INTO`Banque`.`users` (`id`,`login`,`pass`) VALUES ('123456','Alphonse Didle','alphonse'),('234567','Gertrude Stuart','ilovemummy');
 
 INSERT INTO `Banque`.`accounts` (`id`,`idUsers`,`type`,`amount`) VALUES ('987654321','123456','CC','2186.18'),('123456789','123456','CEL','5000.00'),('987456321','123456','LivretJeune','4000.00'),('963258741','234567','CC','541.23'),('654321987','234567','CEL','9874.00');
